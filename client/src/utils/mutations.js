6b661cb60f000fe027e42d6a35e0d75372940c98
Sean <seanms418@gmail.com>
2022-05-01 16:35:21 +1000

    added save_book mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,3 +24,16 @@ export const ADD_USER = gql`
   }
 `;
 
+export const SAVE_BOOK = gql`
+  mutation saveBook($authors: String!, $description: String!, $title: String!, 
+  $bookId: ID!, $image: String!, $link: String!) {
+    saveBook(authors: $authors, description: $description, title: $title, 
+    bookId: $bookId, image: $image, link: $link) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
