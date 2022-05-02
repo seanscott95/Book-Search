757d7d2f00c39ab0002d195dd06a91fe50287fe1
Sean <seanms418@gmail.com>
2022-05-02 14:54:59 +1000

    troubleshooting save book

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -36,8 +36,8 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook($bookId: ID!, $authors: [String]!, $description: String!,
-   $title: String!, $image: String!, $link: String!) {
+  mutation saveBook($bookId: ID!, $authors: [String], $description: String!,
+   $title: String!, $image: String, $link: String) {
     saveBook(bookid: $bookId, authors: $authors, description: $description, title: $title, image: $image, link: $link) {
       _id
       username
